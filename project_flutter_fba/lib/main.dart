711452b83255b017db2d25c471fed94ec4dd1b7b
Alfaisalw <fba.jou11m@gmail.com>
2026-09-09 21:11:46 +0300

    Add new project folder

--- project_flutter_fba/lib/main.dart
+++ project_flutter_fba/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:project_flutter_fba/pages/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async{
+
+Future <void> main() async{
+
+
+
+  await dotenv.load();
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: "https://bbulhmdwqidngfpufina.supabase.co",
-    publishableKey: 'ey',
+    url:dotenv.get('url'),
+    publishableKey: dotenv.get('pupKey'),
   );
 
 
